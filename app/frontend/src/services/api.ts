9cec854ff89225ad45f9f4faa74538695e370de1
Gianluca Abbiati <gianluca@be-digital.it>
2026-06-21 17:15:40 +0200

    Update api.ts

--- app/frontend/src/services/api.ts
+++ app/frontend/src/services/api.ts
@@ -8,7 +8,7 @@ import {
 } from '@/services/types';
 
 /* const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'; */
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://ai-hedge-fund-production-bbbe.up.railway.app';
+const API_BASE_URL = 'https://ai-hedge-fund-production-bbbe.up.railway.app';
 
 export const api = {
   /**
